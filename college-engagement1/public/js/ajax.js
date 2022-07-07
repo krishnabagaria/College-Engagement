cf7176aa17e566d2a2b2d41f69fffd6fa79b8e14
Krishna <krishnabagaria28@gmail.com>
2022-07-07 17:24:19 +0530

    Saving to Github

--- college-engagement1/public/js/ajax.js
+++
@@ -1,3 +0,0 @@
-const onClicked = ()=>{
-    console.log("hello");
-}
--- college-engagement1/routes/profile.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const isAuth = require("../middlewares/isAuth");
-const {getAllProfilePost}=  require("../controllers/profileController");
-
-router.get("/profile",isAuth ,getAllProfilePost);
-
-module.exports = router;
